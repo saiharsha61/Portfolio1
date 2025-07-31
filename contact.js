272f959ece4d2ca14b57f6f851523220acc3a066
saiharsha61 <121154104+saiharsha61@users.noreply.github.com>
2025-07-31 09:37:36 +0530

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thank you! Your message was "sent". (For real email, use a service like Formspree)');
+});
